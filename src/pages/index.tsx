f8c7a0de369697f95a0d80211bd5dc7525ebef45
ktarun1419 <tarunkaushik355@gmail.com>
2024-03-14 23:53:00 +0530

    frame change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import axios from "axios";
+import { FrameMetadata } from '@coinbase/onchainkit/frame';
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
 const inter = Inter({ subsets: ["latin"] });
@@ -37,9 +38,38 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Head>
+    <Head>
+    <FrameMetadata
+      buttons={[
+        {
+          label: 'Tell me the story',
+        },
+        {
+          action: 'link',
+          label: 'Link to Google',
+          target: 'https://www.google.com'
+        },
+        {
+          action: 'post_redirect',
+          label: 'Redirect to cute pictures',
+        },
+      ]}
+      image={{
+       src: 'https://zizzamia.xyz/park-3.png',
+       aspectRatio: '1:1'
+      }}
+      input={{
+        text: 'Tell me a boat story',
+      }}
+      state={{
+        counter: 1,
+      }}
+      postUrl="https://zizzamia.xyz/api/frame"
+    />
+    </Head>
+      {/* <Head>
       <h1>Chad Finance</h1>
-      </Head>
+      </Head> */}
       
     </>
   );
