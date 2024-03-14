b318bc261beb7482c19546139ede166379b3587e
ktarun1419 <tarunkaushik355@gmail.com>
2024-03-14 22:58:36 +0530

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
-
+ const getUserIDs=()=>{
+  try {
+    axios.get('https://nemes.farcaster.xyz/v1/fids').then((res)=>{
+      console.log({res})
+    })
+  } catch (error) {
+    console.log(error)
+  }
+ }
+ getUserIDs()
 export default function Home() {
   return (
     <>
